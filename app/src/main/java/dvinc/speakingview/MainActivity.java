4e36ebd584b27794ced53de2375b709c27232d60
Denis V <decsent@Yandex.ru>
2017-10-26 22:53:14 +0300

    Added simple text-to-speech with play() method.

--- app/src/main/java/dvinc/speakingview/MainActivity.java
+++ app/src/main/java/dvinc/speakingview/MainActivity.java
@@ -2,12 +2,32 @@ package dvinc.speakingview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.dvinc.speakingview.SpeakingView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SpeakingView speakingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        speakingView = findViewById(R.id.speakingView);
+
+        speakingView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakingView.playPhrase("Hello word");
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        speakingView.disposeView();
     }
 }
--- speakingview/src/main/java/com/dvinc/speakingview/SpeakingView.java
+++ speakingview/src/main/java/com/dvinc/speakingview/SpeakingView.java
@@ -1,10 +1,16 @@
 package com.dvinc.speakingview;
 
 import android.content.Context;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
+
+import java.util.Locale;
 
 /**
  *
@@ -12,10 +18,37 @@ import android.widget.FrameLayout;
  */
 
 public class SpeakingView extends FrameLayout {
-    
+
+    private TextToSpeech textToSpeech;
+    FrameLayout rootView;
+
     public SpeakingView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        FrameLayout root = (FrameLayout) inflate(context, R.layout.speaking_view, this);
+        rootView = (FrameLayout) inflate(context, R.layout.speaking_view, this);
+
+        textToSpeech = new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
     }
 
+    public void playPhrase(@NonNull String text) {
+        if (textToSpeech.isSpeaking()) {
+            textToSpeech.stop();
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "Speak");
+        } else {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+        //TODO animation here?
+    }
+
+    public void disposeView(){
+        textToSpeech.shutdown();
+    }
 }
